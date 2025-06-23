37f6530eab5a56b433d669d45da23d19f218509f
omer4153 <gamestnt08@gmail.com>
2025-06-23 03:19:35 +0300

    Dockerfile and PostgreSQL connection added

--- Program.cs
+++ Program.cs
@@ -5,7 +5,8 @@ using alacakverecektakip.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
